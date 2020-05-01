1cdf3e97892900e161fde003214e0bbe0b356c8e
khj2824 <64399268+khj2824@users.noreply.github.com>
2020-05-01 12:26:45 +0900

    0501
    0501

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -6,13 +6,14 @@ public class Enemy : MonoBehaviour
 {
     public List<Transform> node;
     public TowerManager tower;
-
+    public UIUXManager UIUX;
     public List<float> distance = new List<float>();
     int count = 1;
 
     void Start()
     {
         tower = GameObject.Find("TowerManager").GetComponent<TowerManager>();
+        UIUX = GameObject.Find("UIUXManager").GetComponent<UIUXManager>();
         Transform nodeparent = GameObject.Find("Node").transform;
         for(int i=0;i<nodeparent.childCount;i++)
         {
